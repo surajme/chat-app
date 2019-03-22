04ca94a62a822cdf6aeb56df1c61ae272d514fad
surajme <suraj.success121@gmail.com>
2019-03-23 01:53:43 +0530

    'chat-app'

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const socket = require('socket.io');
+
+// App setup
+const app = express();
+const server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+const io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
